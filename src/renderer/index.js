51c50110ff54227e241479f87e8caf2af7a91464
yl1730 <1512769701@qq.com>
2019-01-23 17:51:05 +0800

    add popup

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
+import '../logger';
 import App from './App.vue';
+import rtc from './rtc';
+import popup from './popup';
+import i18n from './i18n';
 import './plugins/ant-design';
 import './plugins/electron';
 
 Vue.config.productionTip = false;
 
 export default new Vue({
+  rtc,
+  popup,
+  i18n,
   render : (h) => h(App),
 }).$mount('#app');
