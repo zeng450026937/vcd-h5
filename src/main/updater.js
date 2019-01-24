24c10d0f147880f5e3382757b0d2ab5d0f77ae1d
xiewei <xiewei@yealink.com>
2019-01-24 15:57:57 +0800

    update

--- src/main/updater.js
+++ src/main/updater.js
@@ -1,35 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { app, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
+/* eslint-enable import/no-extraneous-dependencies */
 
-import { mainWindow } from './window';
+ipcMain.on('will-quit', () => {
 
-autoUpdater.on('update-available', (info) => {
-  mainWindow.send('update-available', info);
-});
-autoUpdater.on('update-not-available', (info) => {
-  mainWindow.send('update-unavailable', info);
-});
-autoUpdater.on('error', (err) => {
-  mainWindow.send('update-error', err);
-});
-autoUpdater.on('download-progress', (progressObj) => {
-  mainWindow.send('update-progress', progressObj);
-});
-autoUpdater.on('update-downloaded', (info) => {
-  mainWindow.send('update-finished', info);
-});
-
-ipcMain.on('updater-check-update', () => {
-  autoUpdater.checkForUpdates();
-});
-
-ipcMain.on('updater-update', () => {
-  console.warn('updater-update');
-  autoUpdater.checkForUpdatesAndNotify();
-});
-
-
-ipcMain.on('updater-install', () => {
-  autoUpdater.quitAndInstall(true, true);
 });
