2604ea21c17d6d47384b2d03962479aa8dfd3e1f
yl1730 <1512769701@qq.com>
2019-01-24 14:36:34 +0800

    add script

---
+++ script/sync.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const execa = require('execa');
+const path = require('path');
+
+async function main() {
+  await execa('git submodule update', [ '--init', '--recursive' ], { stdio: 'inherit' });
+
+  await Promise.all([
+    'lib/apollo-sip',
+    'lib/apollo-rtc',
+    'lib/vuem',
+    'plugins/vue-cli-plugin-vuem',
+    'plugins/vue-cli-plugin-electron',
+  ].map(async(dir) => {
+    await execa('git fetch --all', [], { cwd: path.resolve(process.cwd(), dir), stdio: 'inherit' });
+    await execa('git checkout master', [], { cwd: path.resolve(process.cwd(), dir), stdio: 'inherit' });
+    await execa('git pull', [], { cwd: path.resolve(process.cwd(), dir), stdio: 'inherit' });
+  }));
+}
+
+main();
