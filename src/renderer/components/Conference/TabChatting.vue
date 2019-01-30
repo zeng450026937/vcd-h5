cdf3c29cf0fa1987382384db2bfaeeb15e2b9cad
yl1730 <1512769701@qq.com>
2019-01-30 08:59:47 +0800

    update chat

--- src/renderer/components/Conference/TabChatting.vue
+++ src/renderer/components/Conference/TabChatting.vue
@@ -9,20 +9,20 @@
           <div>
             <div class="flex items-center">
               <span class="whitespace-no-wrap text-xs leading-tight">发给</span>
-              <a-select defaultValue="lucy" class="w-full ml-2">
-                <a-select-option value="jack">Jack</a-select-option>
-                <a-select-option value="lucy">Lucy</a-select-option>
-                <a-select-option value="disabled">张三</a-select-option>
+              <a-select defaultValue="lucy" v-model="message.to" class="w-full ml-2">
+                <a-select-option value="Jack">Jack</a-select-option>
+                <a-select-option value="Lucy">Lucy</a-select-option>
+                <a-select-option value="张三">张三</a-select-option>
                 <a-select-option value="Yiminghe">yiminghe</a-select-option>
               </a-select>
             </div>
           </div>
           <div class="flex mt-2 h-full">
             <div class="w-5/6 mr-2">
-              <a-textarea placeholder="请输入您将要发送的消息" class="h-full"/>
+              <a-textarea v-model="message.content" placeholder="请输入您将要发送的消息" class="h-full"/>
             </div>
             <div class="w-1/6 bg-grey-light">
-              <a-button type="primary" class="w-full h-full p-0 text-xs">发送</a-button>
+              <a-button type="primary" class="w-full h-full p-0 text-xs" @click="sendMessage">发送</a-button>
             </div>
           </div>
         </div>
@@ -32,12 +32,28 @@
 </template>
 
 <script>
+import { cloneDeep } from 'lodash';
 import TabChattingMessageContent from './TabChattingMessageContent.vue';
 
 export default {
   name       : 'TabChatting',
   components : {
     TabChattingMessageContent,
+  },
+  data() {
+    return {
+      message : {
+        from    : '我',
+        to      : 'Lucy',
+        content : '',
+        date    : '今天',
+      },
+    };
+  },
+  methods : {
+    sendMessage() {
+      this.$model.chat.messageRecordList.push(cloneDeep(this.message));
+    },
   },
 };
 </script>
--- src/renderer/components/Conference/TabChattingMessageContent.vue
+++ src/renderer/components/Conference/TabChattingMessageContent.vue
@@ -4,14 +4,14 @@
       <template v-for="(message, index) in messageRecordList">
         <div :key="index">
           <div class="flex flex-col p-3">
-            <div class="flex itemx-center text-xs leading-tight select-none">
+            <div class="flex items-center text-xs leading-tight select-none">
               <span class="text-indigo">{{message.from}}</span>
               <span style="margin: 0 6px">@</span>
               <span class="text-indigo">{{message.to}}</span>
               <div class="flex flex-grow"></div>
               <span class="text-black-lightest">{{message.date}}</span>
             </div>
-            <div style="margin-top: 6px;" class="bg-grey-light px-2 py-1">
+            <div style="margin-top: 6px;" class="rounded-sm bg-grey-light px-2 py-1">
               <div class="leading-normal">{{message.content}}</div>
             </div>
           </div>
@@ -23,51 +23,22 @@
 
 <script>
 
-const messageRecordList = [
-  {
-    from    : '张三',
-    to      : '所有人',
-    content : '你有时看云，有时看我，我觉得，你看云时很远，看我时很近。',
-    date    : '三天前',
-  },
-  {
-    from    : '李四',
-    to      : '王五',
-    content : '我知道永逝将临，并不悲伤，下面是海，远看像森林，而我是在这中间的，走过的人说树枝低了，走过的人说树枝在长',
-    date    : '16：00',
-  },
-  {
-    from    : '枫城',
-    to      : '简',
-    content : '小巷，又深又长，我拿着把旧钥匙，敲着厚厚的墙',
-    date    : '18：24',
-  },
-  {
-    from    : '枫城',
-    to      : '简',
-    content : '小巷，又深又长，我拿着把旧钥匙，敲着厚厚的墙',
-    date    : '18：24',
-  },
-  {
-    from    : '枫城',
-    to      : '简',
-    content : '小巷，又深又长，我拿着把旧钥匙，敲着厚厚的墙',
-    date    : '18：24',
-  },
-  {
-    from    : '枫城',
-    to      : '简',
-    content : '小巷，又深又长，我拿着把旧钥匙，敲着厚厚的墙',
-    date    : '18：24',
-  },
-];
-
 export default {
   name : 'TabChattingMessageContent',
   data() {
     return {
-      messageRecordList,
+      message : {
+        from    : '我',
+        to      : '陌生人',
+        content : '',
+        date    : '18：24',
+      },
     };
+  },
+  computed : {
+    messageRecordList() {
+      return this.$model.chat.messageRecordList;
+    },
   },
 };
 </script>
