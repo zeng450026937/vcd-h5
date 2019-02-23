b1dba5150cae0681a49a1a54b3fcb23b46b70f64
yl1524 <xiewei@yealink.com>
2019-02-23 15:59:56 +0800

    新增 api 接口

--- src/renderer/service/api/deviceManagement.js
+++ src/renderer/service/api/deviceManagement.js
@@ -4,10 +4,22 @@ const { post, put } = service;
 
 export const clientUpdate = (clientId, data) => put(`softClients/${clientId}`, data);
 
+export const fullAmountUpdate = (clientId, data) => post('/softClients', data);
+
 export const clientHeart = (clientId) => post(`/softClients/${clientId}/heart`);
+
+export const checkRegister = (clientId) => post(`/softClients/${clientId}/checkRegister`);
+
+export const pullLatestPackage = (clientId, data) => post(`/softClients/${clientId}/poll`, data);
+
+export const reportAlarm = (clientId, data) => post(`/softClients/${clientId}/report`, data);
 
 
 export default {
   clientHeart,
   clientUpdate,
+  fullAmountUpdate,
+  checkRegister,
+  pullLatestPackage,
+  reportAlarm,
 };
