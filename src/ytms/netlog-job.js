2e41ce08821577b10961b0ab60a89ee49aad2fcd
zengsj <zengshuangjie@gmail.com>
2019-03-14 08:29:42 +0800

    electron netLog模块无法直接访问属性，改成通过session获取

--- src/ytms/netlog-job.js
+++ src/ytms/netlog-job.js
@@ -1,4 +1,4 @@
-import { netLog } from 'electron';
+import { session } from 'electron';
 import { resolve } from 'path';
 import { readFile, ensureDir } from 'fs-extra';
 import { Job } from './job';
@@ -17,16 +17,36 @@ export class NetLogJob extends Job {
     if (logpath) {
       this.logpath = logpath;
     }
+
+    const { netLog } = session.defaultSession;
+
+    if (netLog.currentlyLogging) {
+      logger.warn('netlog is currently logging, session: %s', this.id);
+
+      return;
+    }
     
     ensureDir(getNetLogDirectoryPath())
       .then(() => {
         netLog.startLogging(this.logpath);
         super.start();
+      })
+      .catch((e) => {
+        logger.error('ensure netlog directory failed, error: %s', e);
       });
   }
 
   stop() {
     if (this.isStop) return;
+
+    const { netLog } = session.defaultSession;
+
+    if (!netLog.currentlyLogging) {
+      logger.warn('netlog is not logging, session: %s', this.id);
+      super.stop();
+
+      return;
+    }
 
     netLog.stopLogging(async(path) => {
       let logfile;
@@ -45,7 +65,8 @@ export class NetLogJob extends Job {
   
       log.addLog(logfile, this.id);
 
-      await log.upload().catch((e) => logger.error(e));
+      await log.upload()
+        .catch((e) => logger.error('failed to upload netlog, error: %s', e));
       
       super.stop();
     });
