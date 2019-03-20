b902551b2d62dcfa246b2ecda8bc891668f07be1
zengjay <zengshuangjie@gmail.com>
2019-03-20 22:36:50 +0800

    更新analyser

--- src/renderer/model/rtc/media/analyser/status-analyser.js
+++ src/renderer/model/rtc/media/analyser/status-analyser.js
@@ -3,22 +3,22 @@ import Analyser from './analyser';
 export const StatusAnalyzer = Analyser.extend({
   data() {
     return {
-      interval : 3000,
-      status   : {
-        active : false,
-        audio  : false,
-        video  : false,
+      status: {
+        active: false,
+        audio: false,
+        video: false,
       },
     };
   },
-  watch : {
-    stream(stream) {
-      if (!stream) return;
+
+  watch: {
+    stream(val) {
+      if (!val) return;
 
       let listener;
 
-      stream.addEventListener('inactive', listener = () => {
-        stream.removeEventListener('inactive', listener);
+      val.addEventListener('inactive', listener = () => {
+        val.removeEventListener('inactive', listener);
 
         this.status.active = false;
         this.status.audio = false;
@@ -26,30 +26,35 @@ export const StatusAnalyzer = Analyser.extend({
       });
     },
   },
-  methods : {
+  
+  methods: {
     analyse() {
       if (this.stream) {
         const audioTracks = this.stream.getAudioTracks();
         const videoTracks = this.stream.getVideoTracks();
 
-        this.status.active = this.stream.active === undefined
-          ? Boolean(audioTracks[0]) && Boolean(videoTracks[0])
-          : this.stream.active;
+        this.status.active = this.stream.active === undefined ?
+          Boolean(audioTracks[0]) && Boolean(videoTracks[0]) :
+          this.stream.active;
 
         if (this.status.active) {
-          this.status.audio = this._checkAvariable(audioTracks);
-          this.status.video = this._checkAvariable(videoTracks);
+          this.status.audio = this.checkAvariable(audioTracks);
+          this.status.video = this.checkAvariable(videoTracks);
         }
-      }
-      else {
+      } else {
         this.status.active = false;
         this.status.audio = false;
         this.status.video = false;
       }
     },
-    _checkAvariable(tracks = []) {
-      return tracks.some((track) => (!('readyState' in track) || track.readyState === 'live')
-              && (!('muted' in track) || track.muted !== true));
+
+    checkAvariable(tracks = []) {
+      return tracks.some((track) => (!('readyState' in track) || track.readyState === 'live') &&
+        (!('muted' in track) || track.muted !== true));
     },
+  },
+
+  created() {
+    this.interval = 3000;
   },
 });
--- src/renderer/model/rtc/media/analyser/volume-analyser.js
+++ src/renderer/model/rtc/media/analyser/volume-analyser.js
@@ -3,73 +3,104 @@ import Analyser from './analyser';
 export const VolumeAnalyzer = Analyser.extend({
   data() {
     return {
-      interval     : 100,
-      volume       : 0,
-      context      : null,
-      streamSource : null,
-      analyser     : null,
+      volume: 0,
     };
   },
-  watch : {
-    enable(enable) {
-      if (enable) {
-        this.createStreamSource();
-      }
-      else {
-        if (this.streamSource) {
-          this.streamSource.disconnect();
-          this.streamSource = null;
-        }
-        if (this.analyser) {
-          this.analyser.disconnect();
-        }
-        this.volume = 0;
-      }
-    },
-    stream(stream) {
-      if (!stream) return;
+
+  watch: {
+    stream(val) {
+      if (!val) return;
+      // reset stream source
       this.createStreamSource();
     },
   },
-  created() {
-    if (window.AudioContext) {
-      this.context = new AudioContext();
-      this.analyser = this.context.createAnalyser();
-      this.analyser.fftSize = 1024;
-      this.analyser.smoothingTimeConstant = 0.5;
-    }
-  },
-  destroyed() {
-    this.context = null;
-    this.analyser = null;
-  },
-  methods : {
+
+  methods: {
     analyse() {
-      if (this.context && this.analyser && this.streamSource) {
-        const length = this.analyser.frequencyBinCount;
-        const array = new Uint8Array(length);
-
-        this.analyser.getByteFrequencyData(array);
-
-        const sum = array.reduce((acc, val) => acc + val, 0);
-
-        this.volume = sum / length;
-      }
-      else {
+      if (!this.context || this.contextAnalyser || this.streamSource) {
         this.volume = 0;
+
+        return;
       }
+      
+      const length = this.contextAnalyser.frequencyBinCount;
+      const array = new Uint8Array(length);
+
+      this.contextAnalyser.getByteFrequencyData(array);
+
+      const sum = array.reduce((acc, val) => acc + val, 0);
+
+      this.volume = sum / length;
     },
+
+    start() {
+      // lazy create context analyser
+      this.createContextAnalyser();
+
+      if (!this.streamSource) return;
+
+      Analyser.options.methods.start.call(this, val);
+    },
+
+    stop() {
+      if (this.streamSource) {
+        this.streamSource.disconnect();
+        this.streamSource = null;
+      }
+
+      this.volume = 0;
+
+      Analyser.options.methods.stop.call(this, val);
+    },
+
     createStreamSource() {
+      if (this.streamSource) {
+        this.streamSource.disconnect();
+        this.streamSource = null;
+      }
+
       if (this.context && this.stream && this.stream.getAudioTracks().length > 0) {
         // createMediaStreamSource will fail if there is no audio track.
         // use try,catch instead will be better?
         this.streamSource = this.context.createMediaStreamSource(this.stream);
-        this.streamSource.connect(this.analyser);
+        this.streamSource.connect(this.contextAnalyser);
 
         if (this.context.state === 'suspended') {
           this.context.resume();
         }
       }
     },
+
+    createContextAnalyser() {
+      if (this.context && this.contextAnalyser) return;
+
+      if (!window.AudioContext) {
+        console.warn('cannot context analyser');
+
+        return;
+      }
+
+      this.context = new AudioContext();
+      this.contextAnalyser = this.context.createAnalyser();
+      this.contextAnalyser.fftSize = 1024;
+      this.contextAnalyser.smoothingTimeConstant = 0.5;
+    },
+  },
+
+  created() {
+    this.streamSource = null;
+    this.context = null;
+    this.contextAnalyser = null;
+  },
+
+  destroyed() {
+    if (this.contextAnalyser) {
+      this.contextAnalyser.disconnect();
+      this.contextAnalyser = null;
+    }
+
+    if (this.context) {
+      this.context = null;
+    }
   },
 });
