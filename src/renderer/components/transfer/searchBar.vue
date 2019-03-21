7a28858220996590dcb8e715b70c264377c84ca4
yl1524 <xiewei@yealink.com>
2019-03-21 13:50:05 +0800

    补充提交

---
+++ src/renderer/components/transfer/searchBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="content">
+    <a-input v-model="text"
+             placeholder="搜索"
+             class="search-area rounded-none border-none">
+      <div slot="suffix">
+        <a-iconfont
+            v-show="text"
+            type="icon-guanbi"
+            class="text-sm text-black9 text-base cursor-pointer mr-2 hover:text-red"
+            @click="text = ''"></a-iconfont>
+        <a-iconfont
+            type="icon-sousuo"
+            class="text-sm text-black9 text-base">
+        </a-iconfont>
+      </div>
+
+    </a-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'searchBar',
+  data() {
+    return {
+      text : '',
+    };
+  },
+  watch : {
+    text(val) {
+      this.$emit('search', val);
+    },
+  },
+  methods : {
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.content {
+  height: 40px;
+  border-bottom: 1px solid #ccc;
+  .search-area{
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
