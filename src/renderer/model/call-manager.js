97331106fbe38fc9b72384f1246373af184011df
zengsj <zengshuangjie@gmail.com>
2019-03-22 13:52:30 +0800

    add call-record/call-manager

--- src/renderer/model/call-manager.js
+++ src/renderer/model/call-manager.js
@@ -2,4 +2,51 @@ import Vuem from './vuem';
 
 const model = new Vuem();
 
+model.provide({
+  data() {
+    return {
+      lastSendTime : null,
+      lastRecvTime : null,
+    };
+  },
+
+  middlseware : {
+    // p2p
+    async connect(ctx, next) {
+      await next();
+    },
+
+    async ipcall(ctx, next) {
+      await next();
+    },
+
+    // conference
+    // & anonymouse dialin
+    async dialin(ctx, next) {
+      await next();
+    },
+
+    async answer(ctx, next) {
+      await next();
+    },
+  },
+
+  async created() {
+    this.outgoing = [];
+    this.outgoing._isVue = true;
+    this.incoming = [];
+    this.incoming._isVue = true;
+
+    await this.$nextTick();
+
+    const account = this.$getVM('account');
+
+    account.$on('newRTCSession', (session) => {
+      this.lastRecvTime = Date.now();
+    });
+
+    this.account = account;
+  },
+});
+
 export default model;
--- src/renderer/model/call-record.js
+++ src/renderer/model/call-record.js
@@ -2,4 +2,26 @@ import Vuem from './vuem';
 
 const model = new Vuem();
 
+model.provide({
+  data() {
+    return {};
+  },
+
+  methods : {
+    onOutgoing() {},
+
+    onIncoming() {},
+
+    onRefuse() {},
+    
+    onNoAnswer() {},
+    
+    onHangup() {},
+    
+    onRefer() {},
+
+    onReplace() {},
+  },
+});
+
 export default model;
