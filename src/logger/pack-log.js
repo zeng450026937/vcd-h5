6230a7b9d7c173e4aef9f2fbed1667aa9f14d40c
zengsj <zengshuangjie@gmail.com>
2019-04-03 13:46:03 +0800

    日志打包前先拷贝

--- src/logger/pack-log.js
+++ src/logger/pack-log.js
@@ -1,16 +1,18 @@
 import archiver from 'archiver';
-import { createWriteStream, ensureDir } from 'fs-extra';
+import { createWriteStream, ensureDir, copy } from 'fs-extra';
 import { resolve as resolvePath } from 'path';
 import { getLogDirectoryPath, getNetLogDirectoryPath } from './get-log-path';
 import { newPlainUUID } from '../shared/uuid';
 
 export function packLog(filename = newPlainUUID(), format = 'zip', gzip = false) {
   const dir = getLogDirectoryPath();
+  const copyDir = resolvePath(dir, '../logs-backup');
 
   return ensureDir(dir)
+    .then(() => copy(dir, copyDir))
     .then(() => new Promise((resolve, reject) => {
       const name = `${filename}.${format}${gzip ? '.gz' : ''}`;
-      const path = resolvePath(dir, name);
+      const path = resolvePath(copyDir, name);
       const output = createWriteStream(path);
 
       const archive = archiver(format, {
@@ -23,7 +25,7 @@ export function packLog(filename = newPlainUUID(), format = 'zip', gzip = false)
 
       archive.pipe(output);
 
-      archive.directory(dir, '/', (entry) => (/\.log/.test(entry.name) ? entry : false));
+      archive.directory(copyDir, '/', (entry) => (/\.log/.test(entry.name) ? entry : false));
 
       archive.finalize();
     }));
