74051c84b62318e3471b72b317f371a69ae1161e
zengsj <zengshuangjie@gmail.com>
2019-05-09 20:58:53 +0800

    更新ci脚本

--- script/build.js
+++ script/build.js
@@ -1,39 +1,36 @@
-const { spawn } = require('child_process');
 const path = require('path');
+const execa = require('execa');
+const fs = require('fs-extra');
+const globby = require('globby');
+const { parallel, series } = require('./parallel');
 
-function execa(command, args, options) {
-  try {
-    const child = spawn(command, args, options);
-
-    return new Promise((resolve, reject) => {
-      child.on('close', (code) => {
-        if (code === 0) {
-          resolve();
-        }
-        else {
-          reject(new Error(`Command "${command} ${args}" failed, code: ${code}`));
-        }
-      });
-
-      child.on('error', (err) => {
-        reject(err);
-      });
-    });
-  }
-  catch (error) {
-    return Promise.reject(error);
-  }
-}
-
-async function build() {
-  // await execa('git submodule update', [ '--init', '--recursive' ], { stdio: 'inherit', shell: true });
-  await execa('yarn install', [ '--force' ], { stdio: 'inherit', shell: true });
+async function main() {
+  // await execa('yarn install', [ '--force' ], { stdio: 'inherit', shell: true });
   await execa('yarn install', [ '--force' ], { 
     cwd   : path.resolve(process.cwd(), 'plugins/vue-cli-plugin-electron'),
     stdio : 'inherit',
     shell : true,
   });
   await execa('yarn build:electron', [], { stdio: 'inherit', shell: true });
+
+  const upload = path.resolve(process.cwd(), 'out/upload');
+
+  await fs.ensureDir(upload);
+
+  const folder = path.resolve(process.cwd(), 'out/setup');
+
+  const files = await globby([ 
+    'package-info.json',
+    'vc-desktop-*',
+    'builder-effective-config.yaml',
+  ], { cwd: folder });
+
+  await parallel(
+    files.map((file) => async() => {
+      await fs.copy(path.resolve(folder, file), path.resolve(upload, file));
+    }),
+    8
+  );
 }
 
-build();
+main();
